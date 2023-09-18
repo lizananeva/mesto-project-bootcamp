c562f345f4ca228854866ea68bbb9adbe2bb3905
Elizaveta Ananeva-Ryashchenko <elizaveta.elenovna@yandex.ru>
2023-09-18 21:26:16 +0300

    feature: написать функционал добавления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -25,9 +25,20 @@ const initialCards = [
   }
 ];
 
-const cardTemplate = document.querySelector('#card').content;
-const cardElement = cardTemplate.querySelector('.photo-grid__item');
-const listElement = document.querySelector('.photo-grid__list');
+//Создание списка карточек
+const cardTemplate = document.querySelector('#card').content; //Шаблон карточки
+const cardElement = cardTemplate.querySelector('.photo-grid__item'); //Элемент карточки
+const cardsList = document.querySelector('.photo-grid__list'); //Контейнер для карточек
+
+//Добавление новой карточки
+const cardAddButton = document.querySelector('.profile__add-button'); //Кнопка добавления карточки
+const cardPopup = document.querySelector('.popup_type_add-card'); //Попап добавления карточки
+const cardForm = cardPopup.querySelector('.form'); //Форма добавления карточки
+const cardName = cardForm.querySelector('.form__input[name=card-name]'); //Поле ввода (Название) в форме добавления карточки
+const cardLink = cardForm.querySelector('.form__input[name=card-link]'); //Поле ввода (Ссылка на картинку) в форме добавления карточки
+
+//Закрытие всех попапов
+const popupCloseButtons = cardPopup.querySelectorAll('.popup__close');
 
 function createCard({name, link}) {
   const card = cardElement.cloneNode(true);
@@ -40,10 +51,43 @@ function createCard({name, link}) {
 }
 
 function addCard(card) {
-  listElement.append(card);
+  cardsList.prepend(card);
 }
 
 initialCards.forEach(el => {
   const newCard = createCard(el);
   addCard(newCard);
 });
+
+function openPopup(popup) {
+  if (!popup.classList.contains('popup_opened')) {
+    popup.classList.add('popup_opened');
+  }
+}
+
+function closePopup(popup) {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+  }
+}
+
+function handleSubmitForm(event) {
+  // сделать универсальным
+  event.preventDefault();
+  const newCard = createCard({name: cardName.value, link: cardLink.value});
+  addCard(newCard);
+  cardName.value = '';
+  cardLink.value = '';
+}
+
+cardAddButton.addEventListener('click', event => {
+  openPopup(cardPopup);
+});
+
+popupCloseButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    closePopup(cardPopup);
+  });
+});
+
+cardForm.addEventListener('submit', handleSubmitForm);
